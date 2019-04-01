244c2822687bbe8e8ae96fcfa9716c8b7d4fbf13
Muhammad <mkamalkayani@gmail.com>
2019-04-01 12:06:33 +0500

    Updates

---
+++ precache-manifest.95548d5da27ffb32515e32a117ad621f.js
@@ -0,0 +1,150 @@
+self.__precacheManifest = [
+  {
+    "revision": "e9dbbe8a693dd275c16d32feb101f1c1",
+    "url": "/portfolio/static/media/roboto-latin-100.e9dbbe8a.woff"
+  },
+  {
+    "revision": "cb73351fb2a2effc6dba",
+    "url": "/portfolio/static/css/main.9d3a7969.chunk.css"
+  },
+  {
+    "revision": "8c2ade503b34e31430d6c98aa29a52a3",
+    "url": "/portfolio/static/media/roboto-latin-900.8c2ade50.woff"
+  },
+  {
+    "revision": "75951c020a05a9cb587c",
+    "url": "/portfolio/static/js/1.75951c02.chunk.js"
+  },
+  {
+    "revision": "ee2a100325fd284f0097",
+    "url": "/portfolio/static/js/runtime~main.ee2a1003.js"
+  },
+  {
+    "revision": "b26dcca6467471bc2b77da0752600500",
+    "url": "/portfolio/static/media/kamal_pic.b26dcca6.png"
+  },
+  {
+    "revision": "755f2d4daecd03f007f473e484bb33f7",
+    "url": "/portfolio/static/media/local_weather.755f2d4d.jpg"
+  },
+  {
+    "revision": "aafd384c035c910b6ac4d31be1767740",
+    "url": "/portfolio/static/media/motivational_quotes.aafd384c.png"
+  },
+  {
+    "revision": "a47b54eb8042e94eefb573dc8f2d66a9",
+    "url": "/portfolio/static/media/wikipedia_viewer.a47b54eb.jpg"
+  },
+  {
+    "revision": "955cb857661864e0a43f454cc6f7ff66",
+    "url": "/portfolio/static/media/acme_web_design.955cb857.jpg"
+  },
+  {
+    "revision": "edbdac286a65fbf6b802b276b876af04",
+    "url": "/portfolio/static/media/innovation_tool.edbdac28.svg"
+  },
+  {
+    "revision": "3a44df1d65abe07dacc7883ab4bf0187",
+    "url": "/portfolio/static/media/ParticipantsList.3a44df1d.png"
+  },
+  {
+    "revision": "987b84570ea69ee660455b8d5e91f5f1",
+    "url": "/portfolio/static/media/roboto-latin-100.987b8457.woff2"
+  },
+  {
+    "revision": "55536c8e9e9a532651e3cf374f290ea3",
+    "url": "/portfolio/static/media/roboto-latin-300.55536c8e.woff2"
+  },
+  {
+    "revision": "5d4aeb4e5f5ef754e307d7ffaef688bd",
+    "url": "/portfolio/static/media/roboto-latin-400.5d4aeb4e.woff2"
+  },
+  {
+    "revision": "285467176f7fe6bb6a9c6873b3dad2cc",
+    "url": "/portfolio/static/media/roboto-latin-500.28546717.woff2"
+  },
+  {
+    "revision": "037d830416495def72b7881024c14b7b",
+    "url": "/portfolio/static/media/roboto-latin-700.037d8304.woff2"
+  },
+  {
+    "revision": "19b7a0adfdd4f808b53af7e2ce2ad4e5",
+    "url": "/portfolio/static/media/roboto-latin-900.19b7a0ad.woff2"
+  },
+  {
+    "revision": "cb73351fb2a2effc6dba",
+    "url": "/portfolio/static/js/main.cb73351f.chunk.js"
+  },
+  {
+    "revision": "6232f43d15b0e7a0bf0fe82e295bdd06",
+    "url": "/portfolio/static/media/roboto-latin-100italic.6232f43d.woff2"
+  },
+  {
+    "revision": "a1471d1d6431c893582a5f6a250db3f9",
+    "url": "/portfolio/static/media/roboto-latin-300.a1471d1d.woff"
+  },
+  {
+    "revision": "d704bb3d579b7d5e40880c75705c8a71",
+    "url": "/portfolio/static/media/roboto-latin-100italic.d704bb3d.woff"
+  },
+  {
+    "revision": "bafb105baeb22d965c70fe52ba6b49d9",
+    "url": "/portfolio/static/media/roboto-latin-400.bafb105b.woff"
+  },
+  {
+    "revision": "210a7c781f5a354a0e4985656ab456d9",
+    "url": "/portfolio/static/media/roboto-latin-300italic.210a7c78.woff"
+  },
+  {
+    "revision": "d69924b98acd849cdeba9fbff3f88ea6",
+    "url": "/portfolio/static/media/roboto-latin-300italic.d69924b9.woff2"
+  },
+  {
+    "revision": "de8b7431b74642e830af4d4f4b513ec9",
+    "url": "/portfolio/static/media/roboto-latin-500.de8b7431.woff"
+  },
+  {
+    "revision": "9680d5a0c32d2fd084e07bbc4c8b2923",
+    "url": "/portfolio/static/media/roboto-latin-400italic.9680d5a0.woff"
+  },
+  {
+    "revision": "d8bcbe724fd6f4ba44d0ee6a2675890f",
+    "url": "/portfolio/static/media/roboto-latin-400italic.d8bcbe72.woff2"
+  },
+  {
+    "revision": "ffcc050b2d92d4b14a4fcb527ee0bcc8",
+    "url": "/portfolio/static/media/roboto-latin-500italic.ffcc050b.woff"
+  },
+  {
+    "revision": "010c1aeee3c6d1cbb1d5761d80353823",
+    "url": "/portfolio/static/media/roboto-latin-700italic.010c1aee.woff2"
+  },
+  {
+    "revision": "510dec37fa69fba39593e01a469ee018",
+    "url": "/portfolio/static/media/roboto-latin-500italic.510dec37.woff2"
+  },
+  {
+    "revision": "846d1890aee87fde5d8ced8eba360c3a",
+    "url": "/portfolio/static/media/roboto-latin-700italic.846d1890.woff"
+  },
+  {
+    "revision": "bc833e725c137257c2c42a789845d82f",
+    "url": "/portfolio/static/media/roboto-latin-900italic.bc833e72.woff"
+  },
+  {
+    "revision": "cf6613d1adf490972c557a8e318e0868",
+    "url": "/portfolio/static/media/roboto-latin-700.cf6613d1.woff"
+  },
+  {
+    "revision": "7b770d6c53423deb1a8e49d3c9175184",
+    "url": "/portfolio/static/media/roboto-latin-900italic.7b770d6c.woff2"
+  },
+  {
+    "revision": "75951c020a05a9cb587c",
+    "url": "/portfolio/static/css/1.c7fbcf74.chunk.css"
+  },
+  {
+    "revision": "6bc6d0a1b3ff8cff3e56a4bbc7d8c616",
+    "url": "/portfolio/index.html"
+  }
+];

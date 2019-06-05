e7c06a55eddb12795576012ea658a5f8aa9c5380
Muhammad <mkamalkayani@gmail.com>
2019-06-05 12:32:13 +0200

    dictionary app image

--- src/assests/img/images.js
+++ src/assests/img/images.js
@@ -1,15 +1,17 @@
-import kamal_pic from './kamal_pic.png';
-import local_weather from './local_weather.jpg';
-import motivational_quotes from './motivational_quotes.png';
-import wikipedia_viewer from './wikipedia_viewer.jpg';
-import acme_web_design from './acme_web_design.jpg';
-import innovation_tool from './innovation_tool.svg';
+import kamal_pic from "./kamal_pic.png";
+import local_weather from "./local_weather.jpg";
+import motivational_quotes from "./motivational_quotes.png";
+import wikipedia_viewer from "./wikipedia_viewer.jpg";
+import acme_web_design from "./acme_web_design.jpg";
+import innovation_tool from "./innovation_tool.svg";
+import dictionary_management from "./dictionary_management.png";
 
 export {
   kamal_pic,
+  dictionary_management,
   innovation_tool,
   local_weather,
   motivational_quotes,
   wikipedia_viewer,
-  acme_web_design,
+  acme_web_design
 };
